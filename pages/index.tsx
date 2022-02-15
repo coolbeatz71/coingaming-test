0dc5752845a55979612e3355ab4cd259dddef6b3
coolbeatz71 <sigmacool@gmail.com>
2022-02-15 02:01:13 +0200

    bug: fix the figure image obstructing the crypto list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
-import Layout from '@components/Layout';
+import Layout from '@components/layout';
 import HomeContainer from '@containers/Home';
 
 const Home: NextPage = () => {
